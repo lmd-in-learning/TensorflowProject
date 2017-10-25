de65bd899745fb9a3f8c2689a1b4f83963a76d24
lmd-in-learning <492556709@qq.com>
2017-10-25 21:48:14 +0800

    add mnist recognization with softmax function

---
+++ gettingStart/startMnist.py
@@ -0,0 +1,31 @@
+#startMnist.py 
+import tensorflow as tf
+import input_data
+
+mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
+
+x = tf.placeholder("float", [None, 784])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+y_ = tf.placeholder("float", [None,10])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+for i in range(1000):
+  batch = mnist.train.next_batch(50)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+sess.close()
